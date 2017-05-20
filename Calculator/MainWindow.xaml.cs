7023f1c7f0cdb41e4189e0b9005865f3670813ab
InvisiblE182 <osobnimail@protonmail.com>
2017-05-20 20:32:44 +0200

    asd

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -51,7 +51,6 @@ namespace Calculator
 
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
-
             this.PreviewKeyDown += new KeyEventHandler(MainWindow_KeyDown);
             tb.Focus();
         }
@@ -62,13 +61,14 @@ namespace Calculator
             
             if (e.Key == Key.Enter)
             {
-                tb.Text = tb.Text.Replace(",", ".");
                 Calculate(tb.Text);
+                tb.Focus();
             }
 
             if (e.Key == Key.Delete)
             {
                 tb.Text = "";
+                tb.Focus();
             }
 
         }
@@ -77,7 +77,6 @@ namespace Calculator
         {
             try { return double.Parse(GetExpression(input)); }
             catch (Exception) { return Counting(GetExpression(input)); }
-
         }
 
         static private string GetExpression(string input)
@@ -165,15 +164,12 @@ namespace Calculator
                         double d;
                         try { d = double.Parse(param); }
                         catch (Exception) { d = 1; }
-                        output += doFunc(fun, d);
                     }
                 }
             }
             while (operStack.Count > 0)
                 output += operStack.Pop() + " ";
-
-            return output;
-            
+            return output; 
         }
 
         private double Counting(string input)
@@ -223,26 +219,22 @@ namespace Calculator
                             case '/': result = b / a; break;
                         }
                         temp.Push(result);
-                        
                     }
                 }
                 tb.Text = result.ToString();
                 tb.SelectionStart = tb.Text.Length;
                 tb.SelectionLength = 0;
                 return result; 
-                /*try { return temp.Peek(); }
-                catch (Exception) { throw new SyntaxException(); }*/
-
             }
-            
-
         }
+
         static private bool IsDelimeter(char c)
         {
             if ((" =".IndexOf(c) != -1))
                 return true;
             return false;
         }
+         
         static private bool IsOperator(char с)
         {
             if (("+-/*^()PC!%".IndexOf(с) != -1))
@@ -257,23 +249,8 @@ namespace Calculator
                 return true;
             return false;
         }
-        static private String doFunc(String fun, double param)
-        {
-            switch (fun)
-            {
-                case "cos": return Math.Cos(param).ToString();
-                case "sin": return Math.Sin(param).ToString();
-                /*case "tg": if (Math.Abs(param % (2 * Math.PI)) == (Math.PI / 2)) throw new TgException(param); else return Math.Tan(param).ToString();
-                case "asin": if (param < -1 || param > 1) throw new ArcSinCosException(param); else return Math.Asin(param).ToString();
-                case "acos": if (param < -1 || param > 1) throw new ArcSinCosException(param); else return Math.Acos(param).ToString();
-                case "atg": return Math.Atan(param).ToString();
-                case "sqrt": if (param < 0) throw new SqrtException(param); else return Math.Sqrt(param).ToString();
-                case "ln": if (param <= 0) throw new LogException(param); else return Math.Log(param).ToString();
-                case "lg": if (param <= 0) throw new LogException(param); else return Math.Log10(param).ToString();
-                */
-                default: return "";
-            }
-        }
+
+
         static private byte GetPriority(char s)
         {
             switch (s)
@@ -290,76 +267,15 @@ namespace Calculator
                 default: return 4;
             }
         }
+
         private static int factorial(int x)
         {
             int i = 1;
             for (int s = 1; s <= x; s++)
                 i = i * s;
-            //if (x < 0) throw new NegativeFactorialException(x);
             return i;
         }
-        /*
-        public class MyException : Exception
-        {
-            public string type;
-        }
-        public class NegativeFactorialException : MyException
-        {
-            public NegativeFactorialException(int x)
-            {
-                this.type = "Math error";
-                MessageBox.Show("Factorial(" + x + ") does not exsists", type, MessageBoxButtons.OK);
-            }   
-        }
-        public class TgException : MyException
-        {
-            public TgException(double x)
-            {
-                this.type = "Math error";
-                MessageBox.Show("Tg(" + x + ") does not exsists", type, MessageBoxButtons.OK);
-            }
-        }
-        public class SqrtException : MyException
-        {
-            public SqrtException(double x)
-            {
-                this.type = "Math error";
-                MessageBox.Show("Sqrt(" + x + ") does not exsists", type, MessageBoxButtons.OK);
-            }
-        }
-        public class DividedByZeroException : MyException
-        {
-            public DividedByZeroException()
-            {
-                this.type = "Math error";
-                MessageBox.Show("Division by zero is impossible", type, MessageBoxButtons.OK);
-            }
-        }
-        public class LogException : MyException
-        {
-            public LogException(double x)
-            {
-                this.type = "Math error";
-                MessageBox.Show("Log(" + x + ") does not exsists", type, MessageBoxButtons.OK);
-            }
-        }
-        public class SyntaxException : MyException
-        {
-            public SyntaxException()
-            {
-                this.type = "Syntax error";
-                MessageBox.Show("You made a mistake", type, MessageBoxButtons.OK);
-            }
-        }
-        public class ArcSinCosException : MyException
-        {
-            public ArcSinCosException(double x)
-            {
-                this.type = "Math error";
-                MessageBox.Show("Acos(or Asin) (" + x + ") does not exsists", type, MessageBoxButtons.OK);
-            }
-        }*/
-
+        
         private void Button_Click_Back(object sender, RoutedEventArgs e)
         {
             if (tb.Text.Length != 0)
@@ -372,73 +288,19 @@ namespace Calculator
         private void Button_Click_Pow(object sender, RoutedEventArgs e)
         {
             tb.Text += "^";
+            tb.Focus();
+        }
+
+        private void Button_Click_pi(object sender, RoutedEventArgs e)
+        {
+            tb.Text += "π";
+            tb.Focus();
+        }
+
+        private void Button_Click_e(object sender, RoutedEventArgs e)
+        {
+            tb.Text += "e";
+            tb.Focus();
         }
     }
 }
-
-/*
-                if (tb.Text[tb.Text.Length - 1].ToString() == "!")
-                {
-                    double Result = Convert.ToDouble(new DataTable().Compute(tb.Text.ToString().Substring(0, tb.Text.Length - 1), null));
-                    double factlim = Result;
-
-                    if (Result == 1)
-                    {
-                        tb.Text = Result.ToString();
-                    } else if (Result >= 0) {
-
-                        for (int i = 1; i < factlim; i++)
-                        {
-                            Result = Result * i;
-
-
-                        }
-                        tb.Text = Result.ToString();
-                    } else {
-                        tb.Text = "MATH ERROR!";
-                    }
-
-                } else if (tb.Text.Any("^".Contains))
-                {
-                    Power(tb.Text);
-                } else {
-                    double Result = Convert.ToDouble(new DataTable().Compute(tb.Text.ToString(), null));
-                    tb.Text = Result.ToString();
-                }
-                */
-
-/*private void Power(string x)
-    {
-        for (int i = 0; i < x.Length - 1; i++)
-        {
-            List<string> before = new List<string>();
-            List<string> exponent = new List<string>();
-            List<string> symbs = new List<string>(new string[] { "+", "-", "*", "/", "("});
-
-            if (x[i].ToString() == "^")
-            {
-                if (x[i + 1].ToString() == "(")
-                {
-                    foreach (string s in symbs)
-                    {
-                        //if najde shodu tak vratit index
-                    }
-                    before.Add(x.Substring(x.IndexOf("+"  "-"), i)); // zast na znamýnku
-                    x = x.Substring(i + 1); // zast na poslední pravý závorce - hledat odzadu?
-                    exponent.Add(x.Substring(i + 1)); //zast na znamínku
-                } else
-                {
-                    before.Add(x.Substring(x.IndexOf("+"  "-"), i)); // zast na znamýnku
-                    x = x.Substring(i + 1); // zast na poslední pravý závorce - hledat odzadu?
-                    exponent.Add(x.Substring(i + 1)); //zast na znamínku
-                }
-
-
-            }
-        }*/
-
-
-
-
-
-
